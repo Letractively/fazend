ff1756ee2d3274615a311879c8c9b63e170e09d6
necromant2005 <necromant2005@e43075d4-3bdf-11de-8950-77ccfa13e4cc>
2009-07-09 09:10:06 +0000

    DB architecture and base test environment

---
+++ test/FaZend/Pos/Fixture/table_classes.php
@@ -0,0 +1,17 @@
+<?php 
+/**
+ *
+ * Copyright (c) FaZend.com
+ * All rights reserved.
+ *
+ * You can use this product "as is" without any warranties from authors.
+ * You can change the product only through Google Code repository
+ * at http://code.google.com/p/fazend
+ * If you have any questions about privacy, please email privacy@fazend.com
+ *
+ * @copyright Copyright (c) FaZend.com
+ * @author necromant2005@gmail.com
+ * @version $Id$
+ * @category FaZend
+ */
+
