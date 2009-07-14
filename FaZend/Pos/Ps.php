2e968c9343bd977c072185d0ad38b7e6d5b9c1ee
necromant2005 <necromant2005@e43075d4-3bdf-11de-8950-77ccfa13e4cc>
2009-07-14 09:00:24 +0000

    Add save with version functionality

---
+++ FaZend/Pos/Ps.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ *
+ * Copyright (c) FaZend.com
+ * All rights reserved.
+ *
+ * You can use this product "as is" without any warranties from authors.
+ * You can change the product only through Google Code repository
+ * at http://code.google.com/p/fazend
+ * If you have any questions about privacy, please email privacy@fazend.com
+ *
+ * @copyright Copyright (c) FaZend.com
+ * @author necromant2005@gmail.com
+ * @version $Id$
+ * @category FaZend
+ */
+
+class FaZend_Pos_Ps
+{
+    protected $_Object = null;
+    
+    public function __construct(FaZend_Pos_Abstract $Object)
+    {
+        $this->_Object = $Object;
+    }
+} 
