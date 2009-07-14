9345e6a37ddd525c67f665deb21d7ab09327fe91
necromant2005 <necromant2005@e43075d4-3bdf-11de-8950-77ccfa13e4cc>
2009-07-14 15:15:21 +0000

    Write phpDocumentor commnet for all classes methods

--- branches/necromant2005/FaZend/Pos/Abstract.php
+++ branches/necromant2005/FaZend/Pos/Abstract.php
@@ -17,22 +17,52 @@
 
 abstract class FaZend_Pos_Abstract implements RecursiveIterator
 {
+    /**
+     * Object properties
+     * @var array
+     */
     protected $_properties = array();
     
+    /**
+     * Unique object id
+     * @var int
+     */
     protected $_id = 0;
     
+    /**
+     * Nameof cuurent object. Used when store in db.
+     * @var string
+     */
     protected $_name = "";
     
-    protected $_Info = null;
-    
+    /**
+     * Flag, who indicate if object changed
+     * @var unknown_type
+     */
     protected $_is_changed = false;
     
-    protected $_Ps = null;
-    
-    protected $_Parent = null;
-    
+    /**
+     * Object properties hash
+     * @var string
+     */
     protected $_hash = "";
     
+    /**
+     * Ps object
+     * @var FaZend_Pos_Ps
+     */
+    protected $_Ps = null;
+    
+    /**
+     * Parent object
+     * @var FaZend_Pos_Abstract
+     */
+    protected $_Parent = null;
+    
+    /**
+     * Build object FaZend_Pos_Abstract
+     * @return FaZend_Pos_Abstract
+     */
     public function __construct() 
     {
         foreach (get_class_vars(get_class($this)) as $name=>$value) {
@@ -41,6 +71,11 @@ abstract class FaZend_Pos_Abstract implements RecursiveIterator
         $this->_Ps   = new FaZend_Pos_Ps($this);
     }
     
+    /**
+     * Magic getter
+     * @param string $name
+     * @return string|FaZend_Pos_Abstract
+     */
     public function __get($name)
     {
         if (!array_key_exists($name, $this->_properties)) return $this->__set($name, new FaZend_Pos_Object());
@@ -50,11 +85,24 @@ abstract class FaZend_Pos_Abstract implements RecursiveIterator
         return $this->_properties[$name];
     }
     
+    /**
+     * Magic setter
+     * @param srting $name
+     * @param string|FaZend_Pos_Abstract $value
+     * @return string|FaZend_Pos_Abstract
+     */
     public function __set($name, $value)
     {
         return $this->__setProperty($name, $value, true);
     }
     
+    /**
+     * Set object property
+     * @param string $name
+     * @param string|FaZend_Pos_Abstract $value
+     * @param bool $changed
+     * @return string|FaZend_Pos_Abstract
+     */
     public function __setProperty($name, $value, $changed=false)
     {
         if (is_array($value)) Zend_Exception::raise("ArrayIllegal", 'Array dissable only objcets extends FaZend_Pos_Abstract', 'FaZend_Pos_Exception');
@@ -65,6 +113,11 @@ abstract class FaZend_Pos_Abstract implements RecursiveIterator
         return $this->_properties[$name] = $value;
     }
     
+    /**
+     * Set Object internal properties from another object.
+     * @param FaZend_Pos_Abstract $Object
+     * @return FaZend_Pos_Abstract
+     */
     public function setPropertiesFromObject(FaZend_Pos_Abstract $Object)
     {
         $this->_properties = array();
@@ -83,6 +136,10 @@ abstract class FaZend_Pos_Abstract implements RecursiveIterator
         return $this;
     }
 
+    /**
+     * Calculate hash from object properties(Used when check changes object)
+     * @return string
+     */
     public function callHash()
     {
         $properties = array();
@@ -92,11 +149,20 @@ abstract class FaZend_Pos_Abstract implements RecursiveIterator
         return md5(serialize($properties));
     }
     
+    /**
+     * Set object hash
+     * @param string $hash
+     * @return string
+     */
     public function setHash($hash)
     {
         return $this->_hash = $hash;
     }
     
+    /**
+     * Get object hash
+     * @return string
+     */
     public function getHash()
     {
         return $this->_hash;
@@ -135,53 +201,92 @@ abstract class FaZend_Pos_Abstract implements RecursiveIterator
     {
         return is_object($this->current()) && ($this->current() instanceof RecursiveIterator);
     }
-    
+
     public function getChildren() 
     {
         return $this->current();
     }
     
+    /**
+     * Set obejct id
+     * @param int $id
+     * @return int
+     */
     public function setId($id)
     {
         $this->_is_changed = true;
         return $this->_id = $id;
     }
     
+    /**
+     * Get object id
+     * @return int
+     */
     public function getId()
     {
         return $this->_id;
     }
     
+    /**
+     * Return true if object exist in db
+     * @return bool
+     */
     public function hasId()
     {
         return (bool)$this->_id;
     }
     
+    /**
+     * Set parent object
+     * @param FaZend_Pos_Abstract $Parent
+     * @return FaZend_Pos_Abstract
+     */
     public function setParent(FaZend_Pos_Abstract $Parent=null)
     {
-        //return $this->_Parent = $Parent;
+        return $this->_Parent = $Parent;
     }
     
+    /**
+     * Get parent object
+     * @return FaZend_Pos_Abstract
+     */
     public function getParent()
     {
         return $this->_Parent;
     }
+    
+    /**
+     * Return true if object has parent
+     * @return unknown_type
+     */
     public function hasParent()
     {
         return $this->_Parent instanceof FaZend_Pos_Abstract;
     }
-    
-    
+
+    /**
+     * Set object name
+     * @param $name
+     * @return string
+     */
     public function setName($name)
     {
         return $this->_name = $name;
     }
     
+    /**
+     * Get obejct name
+     * @return string
+     */
     public function getName()
     {
         return $this->_name;
     }
 
+    /**
+     * Return true if object changed
+     * @return bool
+     */
     public function isChanged()
     {
         if ($this instanceof FaZend_Pos_Null) return false;
@@ -199,6 +304,10 @@ abstract class FaZend_Pos_Abstract implements RecursiveIterator
         return $this->_Ps;
     }
     
+    /**
+     * Set object as chenged
+     * @return bool
+     */
     public function touch()
     {
         return $this->_is_changed=true;
