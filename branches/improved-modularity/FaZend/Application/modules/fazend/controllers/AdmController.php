301edf8449b7d40cbbcbc521c0de49640eb9b41e
yegor256@gmail.com <yegor256@gmail.com@e43075d4-3bdf-11de-8950-77ccfa13e4cc>
2010-04-01 15:15:30 +0000

    adm actions removed

--- branches/improved-modularity/FaZend/Application/modules/fazend/controllers/AdmController.php
+++ branches/improved-modularity/FaZend/Application/modules/fazend/controllers/AdmController.php
@@ -39,88 +39,6 @@ class Fazend_AdmController extends Fazend_PanelAbstractController
     }
         
     /**
-     * Show db schema
-     *
-     * @return void
-     */
-    public function schemaAction() 
-    {
-        try {
-            $adapter = Zend_Db_Table::getDefaultAdapter();
-        } catch (Exception $e) {
-            $this->view->schema = 'There is no database in the project';
-            return;
-        }
-
-        $tables = $adapter->listTables();
-
-        $sql = '';
-        foreach ($tables as $table) {
-
-            try {
-                $row = $adapter->fetchRow("show create table {$table}");
-
-                if (isset($row['Create Table']))
-                    $sql .= $row['Create Table'];
-                elseif (isset($row['Create View']))
-                    $sql .= $row['Create View'];
-                else
-                    $sql .= "error in {$table}";
-
-            } catch (Exception $e) {
-                $sql .= "Failed to retrieve information about '{$table}': " . $e->getMessage();
-            }
-
-            $sql .= "\n\n";    
-        }    
-
-        $this->view->schema = $sql;
-    }
-
-    /**
-     * Show server error log file
-     *
-     * @return void
-     */
-    public function logAction() 
-    {
-        $this->view->filePath = ini_get('error_log');
-
-        // maybe error_log is not set?
-        if ($this->view->filePath) {
-
-            if ($this->_hasParam('clear'))
-                file_put_contents($this->view->filePath, 'cleared on ' . date('m/d/y h:i:s'));
-
-            $this->view->log = file_get_contents($this->view->filePath);
-
-        } else
-            $this->view->log = 'no [phpSettings.error_log] variable set in app.ini';
-    }
-
-    /**
-     * Show content of tables
-     *
-     * @return void
-     */
-    public function tablesAction() 
-    {
-        $adapter = Zend_Db_Table::getDefaultAdapter();
-        $this->view->tables = array_diff($adapter->listTables(), array('changelog'));
-
-        if (!$this->_hasParam('table'))
-            return;
-
-        $this->view->table = $table = $this->_getParam('table');
-
-        eval ("\$retrieve = FaZend_Db_ActiveTable_{$table}::retrieve(); \$iterator = \$retrieve->fetchAll();");
-
-        $this->view->retrieve = $retrieve;
-
-        FaZend_Paginator::addPaginator($iterator, $this->view, $this->_getParamOrFalse('page'));
-    }
-
-    /**
      * Show content of tables
      *
      * @return void
@@ -129,32 +47,6 @@ class Fazend_AdmController extends Fazend_PanelAbstractController
     {
         if ($this->_hasParam('reload'))
             $this->view->squeezePNG()->startOver();
-    }
-
-    /**
-     * Show backup status
-     *
-     * @return void
-     */
-    public function backupAction() 
-    {
-        $this->view->backup = new FaZend_Backup();
-
-        if ($this->_hasParam('clear'))
-            $this->view->backup->clearSemaphore();
-
-        $files = array();    
-        foreach ($this->view->backup->getS3Files() as $object) {
-
-            $info = $this->view->backup->getS3FileInfo($object);
-            $info['name'] = $object;
-
-            $files[] = $info;
-        }
-
-        usort($files, create_function('$a, $b', 'return $a["mtime"] > $b["mtime"];'));
-
-        $this->view->files = $files;
     }
 
     /**
