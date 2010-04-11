55bdd2b5b9dbc7d5fdc78af12e596d9588753c17
yegor256 <yegor256@gmail.com>
2010-04-11 11:19:12 +0000

    typo

--- FaZend/Application/Resource/Fz/deployer.php
+++ FaZend/Application/Resource/Fz/deployer.php
@@ -80,7 +80,7 @@ class FaZend_Application_Resource_fz_deployer extends Zend_Application_Resource_
         
         if ($toDeploy) {
             // make sure it is loaded already
-            $this->_bootstrap->boostrap('db');
+            $this->_bootstrap->bootstrap('db');
             self::$_deployer->setAdapter($this->_bootstrap->getResource('db'));
             self::$_deployer->deploy();
         }
