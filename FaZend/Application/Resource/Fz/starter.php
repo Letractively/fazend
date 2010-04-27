21710f4fdda5a00c2d2aa145819f73cb08cf6eae
yegor256 <yegor256@gmail.com>
2010-04-27 12:43:56 +0000

    typo

--- FaZend/Application/Resource/Fz/starter.php
+++ FaZend/Application/Resource/Fz/starter.php
@@ -50,7 +50,7 @@ class FaZend_Application_Resource_fz_starter extends Zend_Application_Resource_R
     public function init() 
     {
         // we execute starter ONLY in CLI and during testing
-        if (!defined('CLI_ENVIRONMENT') || APPLICATION_ENV !== 'testing') {
+        if (!defined('CLI_ENVIRONMENT') || (APPLICATION_ENV !== 'testing')) {
             return;
         }
         
